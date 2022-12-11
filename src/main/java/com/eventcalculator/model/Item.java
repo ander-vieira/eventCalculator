5c6f64e305872dac95b55b9eb8f66a109c0c1fd1
Ander Vieira <ander.vieira741@gmail.com>
2022-12-11 17:12:12 +0100

    Remove unused item names

--- src/main/java/com/eventcalculator/model/Item.java
+++ src/main/java/com/eventcalculator/model/Item.java
@@ -7,13 +7,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public class Item {
     private int id;
-    private String name;
     private int amount;
     private int neededAmount;
 
     public Item(Item item) {
         this.id = item.getId();
-        this.name = item.getName();
         this.amount = item.getAmount();
         this.neededAmount = item.getNeededAmount();
     }
